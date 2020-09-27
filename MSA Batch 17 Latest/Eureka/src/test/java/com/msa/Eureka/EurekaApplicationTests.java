19bad5857e8153203b36bdf258c86276d1ec5004
vsphanindra <39903155+vsphanindra@users.noreply.github.com>
2020-09-27 15:54:05 +0530

    Add files via upload
    added files

---
+++ MSA Batch 17 Latest/Eureka/src/test/java/com/msa/Eureka/EurekaApplicationTests.java
@@ -0,0 +1,12 @@
+/*
+ * package com.msa.Eureka;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class EurekaApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
